aacd54aaa42e357b449ff6dabc5af6f87264f87d
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2019-01-21 13:31:11 +0000

    my new commit after chapter 4!

--- rango/views.py
+++ rango/views.py
@@ -11,5 +11,7 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page"
-    "</br> <a href='/rango/'>Index</a>")
+    # return HttpResponse("Rango says here is the about page"
+    # "</br> <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/about.html', context=context_dict)
