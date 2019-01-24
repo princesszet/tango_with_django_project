f90cec4969c99ca9bb6c1cb7c05582c7d5ef81b1
Zuzanna Janeczko <2262189J@student.gla.ac.uk>
2019-01-24 22:59:38 +0000

    Chapter 6 without exercises.

--- populate_rango.py
+++ populate_rango.py
@@ -15,37 +15,37 @@ def populate():
 
     python_pages = [
         {"title": "Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"
+         "url":"http://docs.python.org/2/tutorial/",
          "views": 40},
         {"title":"How to Think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"
+         "url":"http://www.greenteapress.com/thinkpython/",
          "views": 50},
         {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"
+         "url":"http://www.korokithakis.net/tutorials/python/",
          "views": 70} ]
 
     django_pages = [
         {"title":"Official Django Tutorial",
-         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/",
          "views": 90},
         {"title":"Django Rocks",
-         "url":"http://www.djangorocks.com/"
+         "url":"http://www.djangorocks.com/",
          "views": 70},
         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"
+         "url":"http://www.tangowithdjango.com/",
          "views": 20} ]
 
     other_pages = [
         {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"
+         "url":"http://bottlepy.org/docs/dev/",
          "views": 30},
         {"title":"Flask",
-         "url":"http://flask.pocoo.org"
+         "url":"http://flask.pocoo.org",
          "views": 60} ]
 
     cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
             "Django": {"pages": django_pages, "views": 64, "likes": 32},
-            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     # If you want to add more catergories or pages,
     # add them to the dictionaries above.
